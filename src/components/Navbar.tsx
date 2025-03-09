319f6bdda746ab2e6745651395a002dc4ada3715
Leo Constantin <lcon69184@gmail.com>
2025-03-09 08:41:39 -0700

    added sidebar component

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -7,7 +7,7 @@ import { syncUser } from '@/actions/user.action';
 
 export default async function Navbar() {
     const user = await currentUser();
-    if(user) await syncUser();
+    if(user) await syncUser(); // Post request 
   
   return (
     <nav className="sticky top-0 w-full border-b bg-background/95 backdrop-blur supports-[backdrop-filter]:bg-background/60 z-50">

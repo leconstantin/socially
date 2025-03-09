6b656955ddf74c75e705e4a6e8cf2947eafeefa8
Leo Constantin <lcon69184@gmail.com>
2025-03-09 04:46:31 -0700

    added authentication

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,23 @@
+import { Button } from "@/components/ui/button";
+import { SignedIn, SignedOut, SignInButton, SignUpButton, UserButton } from "@clerk/nextjs";
 
 export default function Home() {
   return (
-    <div>
-     Hello
+    <div className="m-4">
+      <SignedOut>
+        <SignInButton mode="modal">
+          <Button>
+            Sign In
+          </Button>
+        </SignInButton>
+        {/* <SignUpButton mode="modal"/> */}
+      </SignedOut>
+      <SignedIn>
+        <UserButton />
+      </SignedIn>
+      <Button variant={"secondary"}>
+        Click me
+      </Button>
     </div>
   );
 }

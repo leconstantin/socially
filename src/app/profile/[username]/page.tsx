cc354716b907dd0e1d6936d24feaacbe492b26e0
Leo Constantin <lcon69184@gmail.com>
2025-03-11 08:42:54 -0700

    working on profile page

--- src/app/profile/[username]/page.tsx
+++ src/app/profile/[username]/page.tsx
@@ -1,7 +1,34 @@
-import React from 'react'
+import { getProfileByUsername, getUserLikedPosts, getUserPosts, isFollowing } from '@/actions/profile.action';
+import { notFound } from 'next/navigation';
+import ProfilePageClient from './ProfilePageClient';
 
-export default function page({ params }: { params: { username: string } }) {
+// Custom metadata
+
+export async function generateMetadata({ params }: { params: { username: string } }) {
+  const user = await getProfileByUsername(params.username);
+  if (!user) return;
+
+  return {
+    title: `Profile | ${user.name ?? user.username}`,
+    description: user.bio || `Check out ${user.username}'s profile.`,
+  };
+}
+
+export default  async function page({ params }: { params: { username: string } }) {
+  const user = await getProfileByUsername(params.username);
+  if (!user) notFound();
+
+  const [posts, likedPosts, isCurrentUserFollowing] = await Promise.all([
+    getUserPosts(user.id),
+    getUserLikedPosts(user.id),
+    isFollowing(user.id),
+  ]);
   return (
-    <div>Profile of {params.username}</div>
+    <ProfilePageClient
+    user={user}
+    posts={posts}
+    likedPosts={likedPosts}
+    isFollowing={isCurrentUserFollowing}
+  />
   )
 }
